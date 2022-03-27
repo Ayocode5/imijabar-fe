39c7d72bff677cce681b91344cc9c8e97510dc9a
osenbal <enbal.blogs.215@gmail.com>
2022-03-28 00:50:31 +0700

    fix scroll bar commite page

--- assets/js/DOM.js
+++ assets/js/DOM.js
@@ -1,3 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('test')
+    const ele = document.querySelector('.scroll_content_page');
+    console.log(ele)
+    ele.style.cursor = 'grabbing';
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    const mouseDownHandler = function (e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            top: ele.scrollTop,
+            // Get the current mouse position
+            x: e.clientX,
+            y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+
+        // Scroll the element
+        ele.scrollTop = pos.top - dy;
+        ele.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function () {
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    // Attach the handler
+    ele.addEventListener('mousedown', mouseDownHandler);
+});
+
+
+
 $('.carousel_kepengurusan').owlCarousel({
     loop: true,
     margin: 20,
